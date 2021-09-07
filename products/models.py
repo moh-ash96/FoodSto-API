6892f679d1f47189d95d2859d6a532929b680048
moh-ash96 <moh.ash96@gmail.com>
2021-09-08 01:34:05 +0300

    added price to the model

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     image = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
