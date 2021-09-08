13e4c8ee016cc5912939c1fec93fe63e13f3a340
moh-ash96 <moh.ash96@gmail.com>
2021-09-08 11:30:28 +0300

    api is restful and added urls, views, and serializers

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
